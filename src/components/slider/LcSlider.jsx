e24390173926c18b6caa3f0f19bb81099759a117
franklin <igahfranklyn@gmail.com>
2023-08-14 04:33:48 +0100

    slider

---
+++ src/components/slider/LcSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SliderWidget from './';
+const LcSlider = () => {
+  return (
+    <div>
+      <SliderWidget/>
+    </div>
+  )
+}
+
+export default LcSlider
--- src/pages/OpenSourceLicenseCompatibility.js
+++ src/pages/OpenSourceLicenseCompatibility.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '../components/button';
 
 const OpenSourceLicenseCompatibility = () => {
   return (
     <div>
       This is Open Source License Compatibility Screen
+      <Button>hello button</Button>
     </div>
   )
 }
